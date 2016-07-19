a8dbe62027d269b3a62eb616a283d0643084ab36
Karol <Comarch@PC-KW>
2016-07-19 12:01:40 +0200

    Do Rozdzialu 03 03

---
+++ Shop/Shop/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public void Index()
+        {
+
+        }
+        [ActionName("Pokaz")]
+        public ActionResult Display(int id)
+        {
+            throw new Exception("Coś poszło nie tak");
+            Product testProduct = new Product();
+            testProduct.Id = 1;
+            testProduct.Name = "Ksiązka MVC";
+            testProduct.Description = "Dużo wiedzy";
+            testProduct.Price = 100;
+
+            if (id == testProduct.Id)
+                return View("~/Views/Product/Display.cshtml",testProduct);
+            else return RedirectToAction("Index","Home");
+        }
+
+
+    }
+}
