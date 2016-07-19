0a02408da344cf29260b6c38dd19f1b0153767e7
Karol <Comarch@PC-KW>
2016-07-19 16:52:00 +0200

    Koniec 3 rozdziału

--- Shop/Shop/Controllers/ProductController.cs
+++ Shop/Shop/Controllers/ProductController.cs
@@ -9,26 +9,91 @@ namespace Shop.Controllers
 {
     public class ProductController : Controller
     {
+        static List<Product> products = new List<Product>() {
+            new Product() { Description="Dużo Widzy", Id=1, Name="myszka", Price=10  },
+             new Product() { Description="Dużo Widzy", Id=2, Name="klawiatura", Price=10  },
+              new Product() { Description="Dużo Widzy", Id=3, Name="ekran", Price=10  },
+               new Product() { Description="Dużo Widzy", Id=4, Name="Dysk", Price=10  }
+        };
+
         // GET: Product
-        public void Index()
+        public ActionResult Index()
         {
-
-        }
-        [ActionName("Pokaz")]
-        public ActionResult Display(int id)
-        {
-            throw new Exception("Coś poszło nie tak");
-            Product testProduct = new Product();
-            testProduct.Id = 1;
-            testProduct.Name = "Ksiązka MVC";
-            testProduct.Description = "Dużo wiedzy";
-            testProduct.Price = 100;
-
-            if (id == testProduct.Id)
-                return View("~/Views/Product/Display.cshtml",testProduct);
-            else return RedirectToAction("Index","Home");
+            var _model = products;
+            return View(_model);
         }
 
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            return View(product);
+        }
 
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(Product collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Product collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Product collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
---
+++ Shop/Shop/Filters/LogAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Filters
+{
+    public class LogAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+        }
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
