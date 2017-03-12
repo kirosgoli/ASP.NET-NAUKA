0ca3ef77e8f15085807db62564bde9c1af47565f
DESKTOP-E6EFMF2\Karol Walczyna <kirosgoli@outlook.com>
2017-03-12 23:08:21 +0100

    asdfdsf

--- Shop/Shop/Models/Product.cs
+++ Shop/Shop/Models/Product.cs
@@ -10,6 +10,7 @@ namespace Shop.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public decimal Price { get; set; } 
+        public decimal Price { get; set; }
+        public int MyProperty { get; set; }
     }
 }
